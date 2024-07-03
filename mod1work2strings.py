1f1a159095f3ca909c1d5e74cbb9d9255f8fcc18
tatiana767 <everydreamwillcomereal@gmail.com>
2024-07-03 20:39:25 +0300

    Add files via upload

---
+++ mod1work2strings.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+   # print(f'Hi, {name}');  # Press ⌘F8 to toggle the breakpoint.
+#Выполните следующие действия в PyCharm:
+#В переменную example запишите любую строку.
+#Выведите на экран(в консоль) первый символ этой строки.
+#Выведите на экран(в консоль) последний символ этой строки (используя отрицательный индекс).
+#Выведите на экран(в консоль) вторую половину этой строки (С нечётным количеством символов: 'Urban' -> 'ban').
+#Выведите на экран(в консоль) это слово наоборот.
+#Выведите на экран(в консоль) каждый второй символ этой строки. (Пример: 'Топинамбур'->'оиабр')*/
+
+
+example = 'education'
+print(example[0])
+print(example[-1])
+print(example[len(example)//2:])
+print(example[::-1])
+print(example[1::2])
+
+
+
+# Press the green button in the gutter to run the script.
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
