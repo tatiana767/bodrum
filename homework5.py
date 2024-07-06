5c29746ed54c661151b38952eb6ca7850b5a301d
tatiana767 <everydreamwillcomereal@gmail.com>
2024-07-06 14:14:48 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,16 @@
+
+
+immutable_var = 'hello', 777, [1,3,4], 'good bye'
+print( immutable_var)
+immutable_var[2][0] = 4
+print( immutable_var)
+#immutable_var[1] = 'hi'
+#print( immutable_var)
+
+
+
+mutable_list = [1, 2, 'tatitana', 'house']
+print(mutable_list)
+
+mutable_list[2] = 'valeria'
+print(mutable_list)
