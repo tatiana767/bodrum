6043fe8dd8d3ea5b97a749490575b27aae9cd189
tatiana767 <everydreamwillcomereal@gmail.com>
2024-07-09 11:54:11 +0300

    Add files via upload

---
+++ module2_2.py
@@ -0,0 +1,21 @@
+'''Задача "Все ли равны?":
+На вход программе подаются 3 целых числа и записываются в переменные first, second и third соответственно.
+Ваша задача написать условную конструкцию (из if, elif, else), которая выводит кол-во одинаковых чисел среди 3-х введённых.
+
+Пункты задачи:
+Если все числа равны между собой, то вывести 3
+Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
+Если равных чисел среди 3-х вообще нет, то вывести 0'''
+
+first = int(input('input first number'))
+second = int(input('input second number'))
+third = int(input('input third number'))
+
+if first == second == third :
+    print('3')
+elif first == second or second == third or first == third :
+    print('2')
+else :
+    print('0')
+
+
