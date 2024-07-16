e7b5b766cc189551a2c3380130871081415ce322
tatiana767 <everydreamwillcomereal@gmail.com>
2024-07-16 13:07:03 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divine(first, second):
+
+    if second == 0:
+        return 'Error'
+    else :
+        return first/second
---
+++ true_math.py
@@ -0,0 +1,8 @@
+import math
+
+def divine(first, second):
+
+    if second == 0:
+        return math.inf
+    else :
+        return first/second
