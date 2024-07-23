8aa23b02e7f8b4332acdd848058169e7a41ca5f4
tatiana767 <everydreamwillcomereal@gmail.com>
2024-07-23 18:53:55 +0300

    Add files via upload

---
+++ module6_3.py
@@ -0,0 +1,70 @@
+'''Необходимо написать 3 класса:
+Horse - класс описывающий лошадь. Объект этого класса обладает следующими атрибутами:
+x_distance = 0 - пройденный путь.
+sound = 'Frrr' - звук, который издаёт лошадь.
+И методами:
+run(self, dx), где dx - изменение дистанции, увеличивает x_distance на dx.
+
+Eagle - класс описывающий орла. Объект этого класса обладает следующими атрибутами:
+y_distance = 0 - высота полёта
+sound = 'I train, eat, sleep, and repeat' - звук, который издаёт орёл (отсылка)
+И методами:
+fly(self, dy) где dy - изменение дистанции, увеличивает y_distance на dy.
+
+Pegasus - класс описывающий пегаса. Наследуется от Horse и Eagle в том же порядке.
+Объект такого класса должен обладать атрибутами классов родителей в порядке наследования.
+Также обладает методами:
+move(self, dx, dy) - где dx и dy изменения дистанции. В этом методе должны запускаться наследованные методы run и fly соответственно.
+get_pos(self) возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance) в том же порядке.
+voice - который печатает значение унаследованного атрибута sound.
+Пункты задачи:
+Создайте классы родители: Horse и Eagle с методами из описания.
+Создайте класс наследник Pegasus с методами из описания.
+Создайте объект класса Pegasus и вызовите каждый из ранее перечисленных методов, проверив их работу.
+'''
+
+
+class Horse:
+    x_distance = 0
+    def __init__(self, distance = 0, sound = 'Frrr'):
+        self.x_distance = distance
+        self.sound = sound
+        super().__init__()
+
+    def run(self, dx):
+        self.x_distance += dx
+
+
+class Eagle:
+    y_distance = 0
+    def __init__(self, distance=0, sound='I train, eat, sleep, and repeat'):
+        self.y_distance = distance
+        self.sound = sound
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+class Pegasus(Horse, Eagle):
+
+    def move(self, dx, dy):
+        super().run(dx)
+        super().fly(dy)
+
+    def get_pos(self):
+        return (self.x_distance, self.y_distance)
+
+    def voice(self):
+        print(self.sound)
+
+
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
