1d146cada515cf688e6f23a3de891dd96b345f5b
tatiana767 <everydreamwillcomereal@gmail.com>
2024-07-29 14:44:54 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,35 @@
+'''import arcade
+
+class Game(arcade.Window):
+    def on_draw(self):
+        self.clear(255,255,255)
+
+
+if __name__ == '__main__':
+    Window = Game(800,600,'Pong game')
+    arcade.run()'''
+
+import arcade
+
+# Set constants for the screen size
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+# Open the window. Set the window title and dimensions (width and height)
+arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing Example")
+
+# Set the background color to white.
+# For a list of named colors see:
+# http://arcade.academy/arcade.color.html
+# Colors can also be specified in (red, green, blue) format and
+# (red, green, blue, alpha) format.
+arcade.set_background_color( arcade.color.WHITE)
+
+# Start the render process. This must be done before any drawing commands.
+arcade.start_render()
+
+# Draw the face
+x = 300
+y = 300
+radius = 200
+arcade.draw_circle_filled( x, y, radius, arcade.color.YELLOW)
