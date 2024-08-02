b77fe3b136c8b3a89f0bab8b42ba93e6f9a561e3
tatiana767 <everydreamwillcomereal@gmail.com>
2024-08-02 14:42:21 +0300

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,84 @@
+'''Задача "Найдёт везде":
+Напишите класс WordsFinder, объекты которого создаются следующим образом:
+WordsFinder('file1.txt, file2.txt', 'file3.txt', ...).
+Объект этого класса должен принимать при создании неограниченного количество названий файлов и записывать их в атрибут file_names в виде списка или кортежа.
+
+Также объект класса WordsFinder должен обладать следующими методами:
+get_all_words - подготовительный метод, который возвращает словарь следующего вида:
+{'file1.txt': ['word1', 'word2'], 'file2.txt': ['word3', 'word4'], 'file3.txt': ['word5', 'word6', 'word7']}
+Где:
+'file1.txt', 'file2.txt', ''file3.txt'' - названия файлов.
+['word1', 'word2'], ['word3', 'word4'], ['word5', 'word6', 'word7'] - слова содержащиеся в этом файле.
+Алгоритм получения словаря такого вида в методе get_all_words:
+Создайте пустой словарь all_words.
+Переберите названия файлов и открывайте каждый из них, используя оператор with.
+Для каждого файла считывайте единые строки, переводя их в нижний регистр (метод lower()).
+Избавьтесь от пунктуации [',', '.', '=', '!', '?', ';', ':', ' - '] в строке. (тире обособлено пробелами, это не дефис в слове).
+Разбейте эту строку на элементы списка методом split(). (разбивается по умолчанию по пробелу)
+В словарь all_words запишите полученные данные, ключ - название файла, значение - список из слов этого файла.
+
+find(self, word) - метод, где word - искомое слово. Возвращает словарь, где ключ - название файла, значение - позиция первого такого слова в списке слов этого файла.
+count(self, word) - метод, где word - искомое слово. Возвращает словарь, где ключ - название файла, значение - количество слова word в списке слов этого файла.
+В методах find и count пользуйтесь ранее написанным методом get_all_words для получения названия файла и списка его слов.
+Для удобного перебора одновременно ключа(названия) и значения(списка слов) можно воспользоваться методом словаря - item().
+
+for name, words in get_all_words().items():
+  # Логика методов find или count'''
+
+class WordsFinder:
+    file_names = []
+
+    def __init__(self, *argv):
+        for i in argv:
+            self.file_names.append(i)
+
+    def get_all_words(self):
+
+        all_words = {}
+        for i in self.file_names:
+            with open(i, encoding='utf-8') as file:
+                str_ = ''
+                for line in file:
+                    str_ += line.replace(',.!?:; - =','').lower()
+
+        all_words[i] = list(str_.split())
+        return all_words
+
+    def find(self, word):
+        dict_ = {}
+        for name, words in self.get_all_words().items():
+            for i in range(0, len(words)):
+                 #print(words[i])
+                 if words[i].lower() == word.lower() :
+                     #print('tut')
+                     dict_[name] = i+1
+                     break
+        return dict_
+
+    def count(self, word):
+        dict_={}
+        for name, words in self.get_all_words().items():
+
+            if words.count(word.lower()) >0 :
+                dict_[name] = words.count(word.lower())
+        return dict_
+
+
+
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+
+
+
+
+
+
+
+
+
+
+
+
