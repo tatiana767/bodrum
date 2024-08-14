f6e3859555d0fe050f15f4949994a2842ef28283
tatiana767 <everydreamwillcomereal@gmail.com>
2024-08-14 12:53:57 +0300

    Add files via upload

---
+++ modile_10_1.py
@@ -0,0 +1,59 @@
+'''Необходимо создать функцию wite_words(word_count, file_name), где word_count - количество записываемых слов, file_name - название файла, куда будут записываться слова.
+Функция должна вести запись слов "Какое-то слово № <номер слова по порядку>" в соответствующий файл с
+прерыванием после записи каждого на 0.1 секунду.
+Сделать паузу можно при помощи функции sleep из модуля time, предварительно импортировав её: from time import sleep.
+В конце работы функции вывести строку "Завершилась запись в файл <название файла>".
+
+После создания файла вызовите 4 раза функцию wite_words, передав в неё следующие значения:
+10, example1.txt
+30, example2.txt
+200, example3.txt
+100, example4.txt
+После вызовов функций создайте 4 потока для вызова этой функции со следующими аргументами для функции:
+10, example5.txt
+30, example6.txt
+200, example7.txt
+100, example8.txt
+Запустите эти потоки методом start не забыв, сделать остановку основного потока при помощи join.
+Также измерьте время затраченное на выполнение функций и потоков. Как это сделать рассказано в лекции к домашнему заданию.'''
+
+from time import sleep
+from datetime import datetime
+from threading import Thread
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as file:
+       for i in range(0,word_count):
+           file.write('какое то слово N' + str(i)+ '\n')
+           sleep(0.1)
+
+
+time_start = datetime.now()
+
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+time_end = datetime.now()
+print(time_end - time_start)
+
+
+thread1 = Thread(target = write_words, args = (10, 'example5.txt'))
+thread2 = Thread(target =  write_words, args =(30, 'example6.txt'))
+thread3 = Thread(target = write_words, args =(200, 'example7.txt'))
+thread4 = Thread(target = write_words, args =(100, 'example8.txt'))
+time_start = datetime.now()
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()
+
+
+time_end = datetime.now()
+print(time_end - time_start)
