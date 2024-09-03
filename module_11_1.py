3483186c17fbaedf751db2c2a6e1cd2fba9f434f
tatiana767 <everydreamwillcomereal@gmail.com>
2024-09-03 14:51:52 +0200

    Update module_11_1.py

--- module_11_1.py
+++ module_11_1.py
@@ -10,13 +10,13 @@ y2 = [i**2 for i in x]
 plt.figure(figsize=(9, 9))
 plt.subplot(5, 1, 1)
 plt.plot(x, y1)               # построение графика
-plt.title("Зависимости: y1 = x, y2 = x^2") # заголовок
-plt.ylabel("y1", fontsize=14) # ось ординат
+plt.title("работа с библиотеками matplotlib и numpy ") # заголовок
+plt.ylabel("y1 = x", fontsize=10) # ось ординат
 plt.grid(True)                # включение отображение сетки
 plt.subplot(5, 1, 2)
 plt.plot(x, y2)               # построение графика
-plt.xlabel("x", fontsize=14)  # ось абсцисс
-plt.ylabel("y2", fontsize=14) # ось ординат
+plt.xlabel("x", fontsize=10)  # ось абсцисс
+plt.ylabel("y2 = x^2", fontsize=10) # ось ординат
 plt.grid(True)                # включение отображение сетки
 #fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
 #plt.grid()
@@ -27,24 +27,28 @@ plt.plot(x, x**2, label='парабола')  # Plot more data on the Axes...
 plt.plot(x, 1000*np.sinc(x), label='1000*sin(x)')  # ... and some more.
 #plt.title("Зависимости:  y = x, y = x^2 ,100*sin(x)")
 plt.xlabel('x label')  # Add an x-label to the Axes.
-plt.ylabel('y label')  # Add a y-label to the Axes.
+plt.ylabel('три графика')  # Add a y-label to the Axes.
 plt.legend()
 plt.subplot(5, 1, 4)
 
 fruits = ["apple", "peach", "orange", "bannana", "melon"]
 counts = [34, 25, 43, 31, 17]
 plt.grid(True)
-plt.bar(fruits, counts, label='диаграмма')
+plt.ylabel('диаграмма')
+plt.bar(fruits, counts)
 plt.legend()
 
 plt.subplot(5, 1, 5)
 ax = np.linspace(0, 1, 5)
 ay = np.linspace(0, 2, 5)
 xg, yg = np.meshgrid(ax, ay)
+plt.ylabel("матрица", fontsize=10)
 plt.plot(xg, yg, color="r", marker="*",  linestyle="none")
 plt.legend()
 plt.grid(True)# Add a legend.
 plt.show()
+
+
 
 
 
