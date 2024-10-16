37a3a5da98dd13fa304d2a76a80c00f817c6e1d3
tatiana767 <everydreamwillcomereal@gmail.com>
2024-10-16 15:29:25 +0100

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,30 @@
+'''Задача "Бот поддержки (Начало)":
+К коду из подготовительного видео напишите две асинхронные функции:
+start(message) - печатает строку в консоли 'Привет! Я бот помогающий твоему здоровью.'
+. Запускается только когда написана команда '/start' в чате с ботом. (используйте соответствующий декоратор)
+all_massages(message) - печатает строку в консоли 'Введите команду /start,
+ чтобы начать общение.'. Запускается при любом обращении не описанном ранее. (используйте соответствующий декоратор)
+Запустите ваш Telegram-бот и проверьте его на работоспособность.
+'''
+
+
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = " "
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+@dp.message_handler(commands = ['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_massages(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
