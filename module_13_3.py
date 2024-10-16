8e5746a654add2f464fc271bb458e972d5cd75a5
tatiana767 <everydreamwillcomereal@gmail.com>
2024-10-16 16:00:24 +0100

    Update module_13_3.py

--- module_13_3.py
+++ module_13_3.py
@@ -16,7 +16,7 @@ api = " "
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage= MemoryStorage())
 
-@dp.message_handler(commands = ['/start'])
+@dp.message_handler(commands = ['start'])
 async def start(message):
     print('Привет! Я бот помогающий твоему здоровью.')
     await message. answer("Привет! Я бот помогающий твоему здоровью")
@@ -25,7 +25,7 @@ async def start(message):
 async def all_messages(message):
     print('Введите команду /start, чтобы начать общение.')
     await message.answer(message.text.upper())
-
+    await message.answer("Введите команду /start, чтобы начать общение.") 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
