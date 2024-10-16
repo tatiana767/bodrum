e17dc5059a730cc365cebdc6ac44d0fff05b5952
tatiana767 <everydreamwillcomereal@gmail.com>
2024-10-16 15:55:26 +0100

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,31 @@
+'''Цель: написать простейшего телеграм-бота, используя асинхронные функции.
+
+Задача "Он мне ответил!":
+Измените функции start и all_messages так, чтобы вместо вывода в консоль строки отправлялись в чате телеграм.
+Запустите ваш Telegram-бот и проверьте его на работоспособность.
+'''
+
+
+
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = " "
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+@dp.message_handler(commands = ['/start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message. answer("Привет! Я бот помогающий твоему здоровью")
+
+@dp.message_handler()
+async def all_messages(message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer(message.text.upper())
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
